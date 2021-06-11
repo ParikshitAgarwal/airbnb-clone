efa90d8f8d98fa2a64f175c5a54f64c011125193
ParikshitAgarwal <parikshitagarwal134@gmail.com>
2021-06-11 11:07:33 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>© 2021 Airbnb clone! No rights
+                reserved - this is a demo!</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+            
+        </div>
+    )
+}
+
+export default Footer
